1c0eb91e9c55493c36b740ecd7ac36b0a6355fc6
austin72905 <Linponggood@gmail.com>
2022-07-28 15:18:05 +0800

    章節3 Test  Array and Collection

--- TestNinja.UnitTests/MathTests.cs
+++ TestNinja.UnitTests/MathTests.cs
@@ -96,5 +96,33 @@ namespace TestNinja.UnitTests
             Assert.That(result, Is.EqualTo(1));
         }
         #endregion
+
+        /*
+            Ordered    check the items are sorted
+            Unique     check the items are not duplicate
+        */
+
+        [Test]
+        public void GetOddNumbers_LimitIsGreaterThanZero_ReturnNumberUpToLimit()
+        {
+            var result=_math.GetOddNumbers(5);
+
+            //Assert.That(result,Is.Not.Empty);
+            //Assert.That(result.Count(), Is.EqualTo(3));
+
+            //Assert.That(result.Count(), Does.Contain(1));
+            //Assert.That(result.Count(), Does.Contain(3));
+            //Assert.That(result.Count(), Does.Contain(5));
+
+            //easy way to write
+            Assert.That(result, Is.EquivalentTo(new [] { 1, 3, 5 }));
+
+            /*
+                want to check the items are sorted or duplicate
+           
+            */
+            //Assert.That(result, Is.Ordered);
+            //Assert.That(result, Is.Unique);
+        }
     }
 }
