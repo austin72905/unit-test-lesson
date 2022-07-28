515a86bd4e876355c07b4d8fe80d950d1e06fc44
austin72905 <Linponggood@gmail.com>
2022-07-28 10:11:09 +0800

    章節2 SetUp and Tear Down

--- TestNinja.UnitTests/MathTests.cs
+++ TestNinja.UnitTests/MathTests.cs
@@ -11,12 +11,33 @@ namespace TestNinja.UnitTests
     [TestFixture]
     public class MathTests
     {
+        private Fundamentals.Math _math;
+        /*
+            每次測試都應該創建一個新的實體
+            但是每次都要寫就很麻煩
+            這時可以用到SetUp、TearDown
+            
+            SetUp每個測試開始前都會執行
+
+            TearDown每個測試結束後都會執行 
+         
+        */
+        //SetUp
+        [SetUp]
+        //方法名隨意
+        public void SetUp()
+        {
+            _math = new Fundamentals.Math();
+        }
+        //TearDown
+
+
         [Test]
         public void Add_WhenCalled_ReturnSumOfArguments()
         {
-            var math=new Fundamentals.Math();
+            //var math=new Fundamentals.Math();
 
-            var result = math.Add(1, 2);
+            var result = _math.Add(1, 2);
 
             Assert.That(result, Is.EqualTo(3));
         }
@@ -24,9 +45,8 @@ namespace TestNinja.UnitTests
         [Test]
         public void Max_FirstArgumentIsGreater_ReturnFirstArgument()
         {
-            var math = new Fundamentals.Math();
 
-            var result=math.Max(2, 1);
+            var result= _math.Max(2, 1);
 
             Assert.That(result, Is.EqualTo(2));
 
@@ -35,9 +55,8 @@ namespace TestNinja.UnitTests
         [Test]
         public void Max_SecondArgumentIsGreater_ReturnSecondArgument()
         {
-            var math = new Fundamentals.Math();
 
-            var result = math.Max(1, 2);
+            var result = _math.Max(1, 2);
 
             Assert.That(result, Is.EqualTo(2));
         }
@@ -45,9 +64,8 @@ namespace TestNinja.UnitTests
         [Test]
         public void Max_ArgumentIsEqual_ReturnSameArgument()
         {
-            var math = new Fundamentals.Math();
 
-            var result = math.Max(1, 1);
+            var result = _math.Max(1, 1);
 
             Assert.That(result, Is.EqualTo(1));
         }
