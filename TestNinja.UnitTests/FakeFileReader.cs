c51334275a90bd3bd1c8a5ba489dd3676cd2f1dd
austin72905 <Linponggood@gmail.com>
2022-08-01 16:34:43 +0800

    章節5  Dependency Injection via Properties

---
+++ TestNinja.UnitTests/FakeFileReader.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests
+{
+    //Fake Mock 都行，沒差
+    public class FakeFileReader : IFileReader
+    {
+        public string Read(string path)
+        {
+            return "";
+        }
+    }
+}
---
+++ TestNinja/Mocking/FileReader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNinja.Mocking
+{
+    /*
+        解偶: 把對外依賴的部分獨立出一個class，方便測試時抽換 
+    */
+    public interface IFileReader
+    {
+        string Read(string path);
+    }
+
+    public class FileReader: IFileReader
+    {
+        public string Read(string path)
+        {
+             return File.ReadAllText(path);
+        }
+    }
+}
