9ffa3e09fe96acb192515c96cb3bd0ebf564edfe
austin72905 <Linponggood@gmail.com>
2022-08-03 22:27:08 +0800

    章節6 Testing InstallerHelper

---
+++ TestNinja.UnitTests/Mocking/InstallerHelperTests.cs
@@ -0,0 +1,53 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class InstallerHelperTests
+    {
+        private Mock<IFileDownloader> _fileDownloader;
+        private InstallerHelper _installerHelper;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fileDownloader = new Mock<IFileDownloader>();
+            _installerHelper = new InstallerHelper(_fileDownloader.Object);
+        }
+
+        [Test]
+        public void DownloaderInstaller_DownloadFails_ReturnFalse()
+        {
+            //你外部傳的一定要是"","" 才會讓他跑Throws WebException
+            //_fileDownloader.Setup(fd => fd.DownloadFile("", "")).Throws<WebException>();
+
+            // 所以要這樣寫，測試才會符合預期，但是這樣寫太死了
+            //_fileDownloader.Setup(fd => fd.DownloadFile("http://example.com/customer/installer", null)).Throws<WebException>();
+
+            _fileDownloader.Setup(fd => 
+                fd.DownloadFile(It.IsAny<string>(), It.IsAny<string>())).Throws<WebException>();
+
+            var result=_installerHelper.DownloadInstaller("customer","installer");
+
+            Assert.That(result, Is.False);
+        }
+
+
+        [Test]
+        public void DownloaderInstaller_DownloadCompletes_ReturnTrue()
+        {
+           
+            var result = _installerHelper.DownloadInstaller("customer", "installer");
+
+            Assert.That(result, Is.True);
+        }
+    }
+}
--- TestNinja/Mocking/InstallerHelper.cs
+++ TestNinja/Mocking/InstallerHelper.cs
@@ -27,6 +27,7 @@ namespace TestNinja.Mocking
             }
             catch (WebException)
             {
+                //其他類型的錯誤就不用測試，因為其他錯誤不應該返回false，應該寫在更外層
                 return false; 
             }
         }
