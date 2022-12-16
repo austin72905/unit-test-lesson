f61ceb2c4c0f55289d019edb1464fdf50c22bb5c
austin72905 <Linponggood@gmail.com>
2022-12-16 15:00:01 +0800

    Writing First Test

--- TestNinja.UnitTests/Mocking/BookingHelperTests.cs
+++ TestNinja.UnitTests/Mocking/BookingHelperTests.cs
@@ -1,9 +1,11 @@
-﻿using NUnit.Framework;
+﻿using Moq;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TestNinja.Mocking;
 
 namespace TestNinja.UnitTests.Mocking
 {
@@ -12,9 +14,31 @@ namespace TestNinja.UnitTests.Mocking
     //如果一個函式友可能很多的結果，可以用這種命名方式
     public class BookingHelper_OverlappingBookingsExistTests
     {
+        [Test]
         public void BookingStartsAndFinishedBeforeAnExistingBooking_ReturnEmptyString()
         {
+            var repository = new Mock<IBookingRepository>();
+            repository.Setup(r => r.GetActiveBookings(1)).Returns(new List<Booking>
+            {
+                new Booking()
+                {
+                    Id=2,
+                    ArrivalDate=new DateTime(2022,12,6,14,0,0),
+                    DepartureDate=new DateTime(2022,12,10,10,0,0),
+                    Reference="a"
+                }
+            }.AsQueryable());
 
+
+            var result=BookingHelper.OverlappingBookingsExist(new Booking 
+            { 
+                Id = 1,
+                ArrivalDate = new DateTime(2022, 12, 1, 14, 0, 0), // 傳入一個預訂日期比較早的
+                DepartureDate = new DateTime(2022, 12, 5, 10, 0, 0),
+            }, 
+            repository.Object);
+
+            Assert.That(result, Is.Empty);
         }
     }
 }
